0b5ec42b135c569fe82ccb3d066d7b1304df28bc
Tord Svensson <tord.svensson@gmail.com>
2019-08-27 08:37:56 +0200

    Made sure only output from failed tests are displayed.

--- src/test/java/gradle_clean_testoutput/LibraryTest.java
+++ src/test/java/gradle_clean_testoutput/LibraryTest.java
@@ -6,18 +6,24 @@ import static org.junit.Assert.*;
 public class LibraryTest {
     
     @Test public void testThatFails() {
+	System.out.println("testThatFails - System.out");
+	System.err.println("testThatFails - System.err");
         Library classUnderTest = new Library();
         assertTrue("returnsFalseShouldReturn should return 'true'", classUnderTest.returnsFalse());
     }
 
 
     @Test public void testThatSucceeds() {
+	System.out.println("testThatSucceeds - System.out");
+	System.err.println("testThatSucceeds - System.err");
         Library classUnderTest = new Library();
         assertFalse("returnsFalse should return 'false'", classUnderTest.returnsFalse());
     }
 
 
     @Test public void testThatThrowsException() {
+	System.out.println("testThatThrowsException - System.out");
+	System.err.println("testThatThrowsException - System.err");
         Library classUnderTest = new Library();
         assertFalse("should return 'false'", classUnderTest.throwsExceptionIfTrueElseFalse(true));
     }
